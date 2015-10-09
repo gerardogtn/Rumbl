4b3d7b584629f4fca8184ac9ce2a7251e605cf77
Gerardo Teruel <gerardogtn@gmail.com>
2015-10-09 16:02:13 -0500

    :rocket: Add repo with user models

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,25 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  #use Ecto.Repo, otp_app: :rumbl
+  @moduledoc """
+  In memory repository.
+  """
+
+  def all(Rumbl.User) do
+    [%Rumbl.User{id: "1", name: "Gerardo", username: "gerardogtn", password: "1234"},
+      %Rumbl.User{id: "2", name: "Almudena", username: "almu", password: "password"},
+      %Rumbl.User{id: "3", name: "Johnny", username: "john", password: "4321"}]
+  end
+
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
+
 end
