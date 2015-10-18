37387745acc1fe150ca2ab2d487d859c641939b9
Gerardo Teruel <gerardogtn@gmail.com>
2015-10-18 12:54:12 -0500

    :rocket: Add login, registration, and user authentication

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,5 +1,6 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  plug :authenticate when action in [:index, :show]
   alias Rumbl.User
 
   def index(conn, _params) do
@@ -13,13 +14,14 @@ defmodule Rumbl.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
 
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
-        |> put_flash(:info, "#{user.name} created!")
-        |> redirect(to: user_path(conn, :index))
+         |> Rumbl.Auth.login(user)
+         |> put_flash(:info, "#{user.name} created!")
+         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
@@ -29,6 +31,17 @@ defmodule Rumbl.UserController do
   def new(conn, _params) do
     changeset = User.changeset(%User{})
     render conn, "new.html", changeset: changeset
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to see this page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
   end
 
 end
